b812425685897ef85893d9f4e8e09938a1b3f5cd
Jasmin Esper <jasmin.09-@live.com.ar>
2023-07-30 22:22:43 -0300

    corrijo error product

--- ProductManager.js
+++ ProductManager.js
@@ -1,48 +1,71 @@
 class ProductManager {
-  product
-  constructor(){
-    this.product=[]
+  #products;
+
+  constructor() {
+    this.#products = [];
   }
 
-get product()
-{
-  return this.product
-}
-
-generateID(){
-  if (this.product.length === 0) return 1
-  return this.product[this.product.length-1].id + 1
-
-}
-
-addProduct (product){
-  if(!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock )
-  return  '[ERR] Required fields missing'
-  const found = this.product.find (item => item.code === product.code)
-  if (found) {
-    return '[ERR] Code already exists'
+  #getNextID() {
+    if (this.#products.length === 0) return 1;
+    return this.#products[this.#products.length - 1].id + 1;
   }
- const productToAdd = {ide: this.generateID(), ...product}
- this.product.push(productToAdd)
- return productToAdd
+  addProduct(product) {
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.code ||
+      !product.stock
+    )
+      return "[ERR] Required fields missing";
+    const found = this.#products.find((item) => item.code === product.code);
+    if (found) {
+      return "[ERR] Code already exists";
+    }
+    const productToAdd = { id: this.#getNextID(), ...product };
+    this.#products.push(productToAdd);
+    return productToAdd;
+  }
+
+  getProducts() {
+    return this.#products;
+  }
+
+  getProductById(id) {
+    const found = this.#products.find((item) => item.id === id);
+    if (!found) return "[ERR] Not Found";
+    return found;
+  }
 }
 
-getProducts (){
-  return this.product
-}
+const pm = new ProductManager();
+console.log(pm.getProducts());
 
-getProductById (id){
-  const found = this.product.find(item => item.id === id)
-  if (!found ) return '[ERR] Not Found'
-  return found
-}
+const product1 = {
+  title: "Maceta",
+  description: "Maceta blanca con flores",
+  price: 2000,
+  thumbnail: "image",
+  code: "01",
+  stock: 10,
+};
 
-}
-const tm = new ProductManager(
-  tm.product = {title:"Maceta", description:"Maceta blanca con flores" price: "2000", thumbnail"", code:"001",stock:"10"}
-  tm.product = {title:"Vela", description:"Vela aromatica" price: "1500", thumbnail"", code:"002",stock:"10"}
-  tm.product = {title:"reloj", description:"Reloj de pared" price: "3000", thumbnail"", code:"003",stock:"10"}
-  tm.product = {title:"Florero", description:"Florero cerramica" price: "2500", thumbnail"", code:"004",stock:"10"}
-  console.log(tm.product)
+const product2 = {
+  title: "Florero",
+  description: "Florero cerámica",
+  price: 2500,
+  thumbnail: "image",
+  code: "02",
+  stock: 10,
+};
 
-)
+pm.addProduct(product1);
+pm.addProduct(product2);
+pm.addProduct("Reloj", "Reloj de pared", 2000, "image", "02", 10); //producto para agregar
+
+//pruebas de en consola
+console.log(pm.getProducts());
+console.log(pm.getProductById(1));
+console.log(pm.getProductById(500));
+console.log(pm.addProduct("Maceta"));
